95b99f6e45d3be2199a3322b54ba6a2b94f0c69b
carrot2803 <lezamadmitri@gmail.com>
2024-04-07 02:17:40 -0400

    delete assignment submission

--- Assignments/Assignment2-BlueJ/PaymentPortalApplication.java
+++
@@ -1,8 +0,0 @@
-// 816035591 Dmitri Lezama
-public class PaymentPortalApplication {
-    public static void main(String[] args) {
-        PaymentSystem paymentSystem = new PaymentSystem();
-        PaymentPortal lennysPaymentPortal = new PaymentPortal(paymentSystem);
-        lennysPaymentPortal.setVisible(true);
-    }
-}
